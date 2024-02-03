87ef0a4157862db8dc6c67017f6ef76c11fe0931
cdnavm <157372155+cdnavm@users.noreply.github.com>
2024-02-03 14:43:08 +0530

    Update generator.js

--- generator.js
+++ generator.js
@@ -42,8 +42,8 @@ function generar() {
         if (isSerie.checked) {
             try {
 
-                const respuesta = await fetch(`https://api.themoviedb.org/3/tv/${serieKey}?api_key=6d9fd0eceda02898513c9454f0b94ccf&language=${languaje}`);
-                const respuesta3 = await fetch(`https://api.themoviedb.org/3/tv/${serieKey}/season/${seasonNumber}?api_key=6d9fd0eceda02898513c9454f0b94ccf&language=${languaje}`);
+                const respuesta = await fetch(`https://api.themoviedb.org/3/tv/${serieKey}?api_key=6d9fd0eceda02898513c9454f0b94ccf&language=en-US`);
+                const respuesta3 = await fetch(`https://api.themoviedb.org/3/tv/${serieKey}/season/${seasonNumber}?api_key=6d9fd0eceda02898513c9454f0b94ccf&language==en-US`);
     
                 if (respuesta.status === 200) {
                     const datos = await respuesta.json();
@@ -192,7 +192,7 @@ function generar() {
         if(isMovie.checked){
             try {
 
-            const respuesta = await fetch(`https://api.themoviedb.org/3/movie/${serieKey}?api_key=6d9fd0eceda02898513c9454f0b94ccf&language=${languaje}`);
+            const respuesta = await fetch(`https://api.themoviedb.org/3/movie/${serieKey}?api_key=6d9fd0eceda02898513c9454f0b94ccf&language=en-US`);
 
             if (respuesta.status === 200) {
                 const datos = await respuesta.json();
